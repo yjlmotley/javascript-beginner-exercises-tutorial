0f6cc9a53011302c8b714b24faa252590edaf143
Yeju Lee Motley <yejuleemotley@gmail.com>
2025-03-20 19:32:28 -0700

    finished tutorial

--- exercises/23-Bottles_of_milk/app.js
+++ exercises/23-Bottles_of_milk/app.js
@@ -1,1 +1,12 @@
 // Your code here:
+function beerOnTheWall() {
+    for(let i = 99; i >= 0; i--) {
+        // console.log(i);
+        if (i == 2) {console.log(`${i} bottles of milk on the wall, ${i} bottles of milk. Take one down and pass it around, ${i - 1} bottle of milk on the wall.`)}
+        else if (i == 1) {console.log(`${i} bottle of milk on the wall, ${i} bottle of milk. Take one down and pass it around, no more bottles of milk on the wall.`)}
+        else if (i == 0) {console.log(`No more bottles of milk on the wall, no more bottles of milk. Go to the store and buy some more, 99 bottles of milk on the wall.`)}
+        else {console.log(`${i} bottles of milk on the wall, ${i} bottles of milk. Take one down and pass it around, ${i - 1} bottles of milk on the wall.`)}
+    }
+}
+
+beerOnTheWall();
